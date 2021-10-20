e0cd60f97fa931ae620fade0c1fbfa881a6b211a
ShashankBanda2 <shashankbanda2@gmail.com>
2021-10-20 10:40:45 +0530

    dw

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Login from "./Login";
+import Signup from "./Signup";
+import './App.css'
+
+function App() {
+  return (
+    <div className="AppComp">
+      <center>
+      <Login/>
+      </center>
+    </div>
+  );
+}
+
+export default App;
